7beb9de20f4e77ea51273f07e03f3097fbd91491
epenflow <epenflow@gmail.com>
2025-02-26 13:44:39 +0800

    issue : can't enable react scan in prod

--- src/components/utils/react-scan.tsx
+++ src/components/utils/react-scan.tsx
@@ -1,17 +1,14 @@
 import { castToBoolean } from '~/lib/utils';
 
 const ReactScan = () => {
+  /**
+   *
+   * Issue -can't enable react scan in prod
+   *
+   */
   return process.env.NODE_ENV === 'development' ||
     castToBoolean(process.env.NEXT_PUBLIC_REACT_SCAN_DEVTOOLS) ? (
-    <>
-      <script async src="https://unpkg.com/react-scan/dist/auto.global.js" />
-      <script
-        dangerouslySetInnerHTML={{
-          __html:
-            'window.ReactScanOptions = { dangerouslyForceRunInProduction: true };'
-        }}
-      />
-    </>
+    <script async src="https://unpkg.com/react-scan/dist/auto.global.js" />
   ) : null;
 };
 
