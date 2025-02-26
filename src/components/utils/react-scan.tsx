6891f54847c66b91ae9802fb5a7f1a3749119971
epenflow <epenflow@gmail.com>
2025-02-26 13:42:36 +0800

    feat : react scan

--- src/components/utils/react-scan.tsx
+++ src/components/utils/react-scan.tsx
@@ -3,14 +3,15 @@ import { castToBoolean } from '~/lib/utils';
 const ReactScan = () => {
   return process.env.NODE_ENV === 'development' ||
     castToBoolean(process.env.NEXT_PUBLIC_REACT_SCAN_DEVTOOLS) ? (
-    <script
-      async
-      src="https://unpkg.com/react-scan/dist/auto.global.js"
-      dangerouslySetInnerHTML={{
-        __html:
-          'window.ReactScanOptions = { dangerouslyForceRunInProduction: true };'
-      }}
-    />
+    <>
+      <script async src="https://unpkg.com/react-scan/dist/auto.global.js" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html:
+            'window.ReactScanOptions = { dangerouslyForceRunInProduction: true };'
+        }}
+      />
+    </>
   ) : null;
 };
 
