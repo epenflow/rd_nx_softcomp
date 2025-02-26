c9c1c1cf011480c9240716259bac9b0830ee83b4
epenflow <epenflow@gmail.com>
2025-02-26 13:38:35 +0800

    feat : enable react scan in production

--- src/components/utils/react-scan.tsx
+++ src/components/utils/react-scan.tsx
@@ -3,7 +3,14 @@ import { castToBoolean } from '~/lib/utils';
 const ReactScan = () => {
   return process.env.NODE_ENV === 'development' ||
     castToBoolean(process.env.NEXT_PUBLIC_REACT_SCAN_DEVTOOLS) ? (
-    <script async src="https://unpkg.com/react-scan/dist/auto.global.js" />
+    <script
+      async
+      src="https://unpkg.com/react-scan/dist/auto.global.js"
+      dangerouslySetInnerHTML={{
+        __html:
+          'window.ReactScanOptions = { dangerouslyForceRunInProduction: true };'
+      }}
+    />
   ) : null;
 };
 
